665649dfdd82981a268d0bd983ac19e9593ba4c8
Kristina <kristyarutya03@mail.ru>
2026-04-11 03:43:14 +0300

    Урок 4, Задача 2: добавлен класс CommandRunner (заглушка) в пакет command_runner
    Изменённые файлы:
    - command_runner/lib/command_runner.dart
    - command_runner/lib/src/command_runner_base.dart
    
    Что сделано:
    1. В command_runner.dart:
       - Объявлена библиотека (library;)
       - Добавлен экспорт src/command_runner_base.dart для публичного доступа
    
    2. В command_runner_base.dart:
       - Создан класс CommandRunner
       - Реализован метод run(List<String> input) async
       - Метод выводит полученные аргументы в консоль (заглушка)
    
    Результат: пакет command_runner готов к подключению в проекте cli.

--- dartpedia/command_runner/lib/command_runner.dart
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -1,8 +1,7 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+// A simple command runner to handle command-line arguments.
+//
+// More extensive documentation for this library goes here.
 library;
 
 export 'src/command_runner_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+// TODO: Export any other libraries intended for clients of this package.
