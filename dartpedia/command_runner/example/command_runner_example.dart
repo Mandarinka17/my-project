33959a01f00d02f7b639d2e9a6b32aa0163f38bc
Kristina <kristyarutya03@mail.ru>
2026-04-11 08:41:32 +0300

    Урок 7, Задачи 3-4: экспорт console.dart и добавлен пример PrettyEcho
    - В command_runner/lib/command_runner.dart добавлен экспорт src/console.dart.
    - Создан пример command_runner/example/command_runner_example.dart с командой PrettyEcho.
    - PrettyEcho раскрашивает каждое слово в разные цвета (titleText, instructionText, errorText).
    - Исправлен синтаксис: убраны именованные аргументы, добавлен обязательный параметр version.

--- dartpedia/command_runner/example/command_runner_example.dart
+++ dartpedia/command_runner/example/command_runner_example.dart
@@ -1,4 +1,55 @@
-/// Пример использования пакета command_runner
-void main() {
-  print('Пример работы command_runner');
+import 'dart:async';
+import 'package:command_runner/command_runner.dart';
+
+class PrettyEcho extends Command {
+  PrettyEcho() {
+    addFlag('blue-only', abbr: 'b', help: 'When true, the echoed text will all be blue.');
+  }
+
+  @override
+  String get name => 'echo';
+  @override
+  bool get requiresArgument => true;
+  @override
+  String get description => 'Print input, but colorful.';
+  @override
+  String? get help => 'Echos a String provided as an argument with ANSI coloring.';
+  @override
+  String? get valueHelp => 'STRING';
+
+  @override
+  FutureOr<String> run(ArgResults arg) {
+    if (arg.commandArg == null) {
+      throw ArgumentException(
+        'This command requires one positional argument',
+        command: name,
+        argumentName: 'text',
+      );
+    }
+
+    final words = arg.commandArg!.split(' ');
+    final prettyWords = <String>[];
+
+    for (int i = 0; i < words.length; i++) {
+      final word = words[i];
+      switch (i % 3) {
+        case 0:
+          prettyWords.add(word.titleText);
+          break;
+        case 1:
+          prettyWords.add(word.instructionText);
+          break;
+        case 2:
+          prettyWords.add(word.errorText);
+          break;
+      }
+    }
+    return prettyWords.join(' ');
+  }
+}
+
+void main(List<String> arguments) {
+  final runner = CommandRunner(version: '0.0.1')
+    ..addCommand(PrettyEcho());   // без command:
+  runner.run(arguments);          // без arguments:
 }
